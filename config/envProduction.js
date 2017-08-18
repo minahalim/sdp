60bdd393ed9fe4042552cbb31511a80e401e1a1c
Mina Halim Hanna <mina.halim@accedo.tv>
2017-08-19 01:19:05 +0800

    more config for the openshift db

--- config/envProduction.js
+++ config/envProduction.js
@@ -8,6 +8,6 @@ module.exports = {
     SERVER_URL: "http://localhost:8080", // Production server URL
 
     googleApiKey: "AIzaSyCc7lkQ8knwThTvGHttJIyByKL4sjvbuWI", // If the key is not the same for the Production
-    mongooseDBURL: process.env.OPENSHIFT_MONGODB_DB_URL, // MongoDB url for the Production
+    mongooseDBURL: process.env.OPENSHIFT_sdp_DB_URL, // MongoDB url for the Production
     databaseName: process.env.OPENSHIFT_APP_NAME	// Database name for the Production
 };
