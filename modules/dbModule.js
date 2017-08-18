632a3ad807c558e9509e0082e7c38fb07133df0e
Mina Halim Hanna <mina.halim@accedo.tv>
2017-08-19 02:01:21 +0800

    Testing new config settings for openshift

--- modules/dbModule.js
+++ modules/dbModule.js
@@ -9,8 +9,19 @@ module.exports = function(config) {
 
 
     function connect() {
+        // default to a 'localhost' configuration:
+        var connectionString = '127.0.0.1:27017/' + config.databaseName;
+        // if OPENSHIFT env variables are present, use the available connection info:
+        if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+          connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+          process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+          process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+          process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+          process.env.OPENSHIFT_APP_NAME;
+        }
+
         mongoose.Promise = global.Promise;
-        mongoose.connect(config.mongooseDBURL + config.databaseName, {
+        mongoose.connect(connectionString, {
             useMongoClient: true
         }, function(error) {
             if (!error) {
